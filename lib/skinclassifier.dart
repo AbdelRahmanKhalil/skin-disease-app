1e4ba8922edb020ac9fb170f4adc41d8dd1dca52
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2022-07-04 19:56:57 +0200

    update machine learning model

--- lib/skinclassifier.dart
+++ lib/skinclassifier.dart
@@ -8,21 +8,22 @@ import 'dart:ui' as ui;
 import 'dart:io' as io;
 
 import 'package:file_picker/file_picker.dart';
-
+import 'package:flutter_native_image/flutter_native_image.dart';
 class skinClassifier{
   skinClassifier();
 
  Future<dynamic> classifyImage(File image) async {
+  ImageProperties properties = await FlutterNativeImage.getImageProperties(image.path);
+  File resized = await FlutterNativeImage.compressImage(image.path, quality: 80,
+    targetWidth: 180, targetHeight: 180);
     await Tflite.loadModel(
       model: "assets/yourmodel.tflite",
-      labels: "assets/labels_skin.txt",
+      labels: "assets/labels_skin2.txt",
     );
     var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 23,
+      path: resized.path,
+      numResults: 8,
       threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5,
     );
     print("fiinished classifying");
     //print(output?[0]['index']);
